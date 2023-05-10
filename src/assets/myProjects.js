ce4ceb1fccd5f79d5346d1f6bc5918e74a9542b9
zodiakara <114649190+zodiakara@users.noreply.github.com>
2023-05-10 21:24:26 +0200

    asset folder add

---
+++ src/assets/myProjects.js
@@ -0,0 +1,13 @@
+const myProjects = [
+  {
+    name: "LinkedIn Clone",
+    duration: "2 weeks",
+    scope: "Frontend and Backend",
+    stack: "React, Redux,",
+    repoFE: "link",
+    repoBE: "",
+    prodFE: "",
+    prodBE: "",
+    description: "",
+  },
+];
