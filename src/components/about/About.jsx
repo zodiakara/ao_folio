8cda064bed52aea2d39d2018b97bfa891877dd0b
zodiakara <114649190+zodiakara@users.noreply.github.com>
2023-05-26 14:18:44 +0200

    icons svg's added to asset folder

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,12 +3,16 @@ import { Box, Typography } from "@mui/material";
 function About() {
   return (
     <Box className="MainContainer">
-      <Typography>aaaaaaaaaaaaaaaaa</Typography>
-      <h1>no i kurwa gdzie to jest</h1>
-      <Typography>
-        I am really passionate about coding, so I might as well get payed to do
-        so.{" "}
-      </Typography>
+      <h1>about me</h1>
+      <Box>
+        <Typography>
+          I am really passionate about coding, so I might as well get payed to
+          do so.{" "}
+        </Typography>
+      </Box>
+      <Box>
+        <h2>Skills</h2>
+      </Box>
     </Box>
   );
 }
